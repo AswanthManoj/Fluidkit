d40fd35dd039086c3499ea7e0ad6158bf557d245
Aswanth <aswanthmanoj51@gmail.com>
2025-07-09 03:05:45 +0530

    feat(config): fluid.config.json file generation and config based code genration control. Stagging for CLI setup

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-from fluidkit import integrate
-from tests.sample.app import app
+# from fluidkit import integrate
+# from tests.sample.app import app
 
-integrate(app)
+# integrate(app)
+
+from fluidkit.core.integrator import test_integration
+from fluidkit.core.config import test_config_management
+from fluidkit.generators.typescript.imports import test_config_driven_imports, test_imports
+from fluidkit.generators.typescript.pipeline import test_config_driven_generation
+
+test_imports()
+# test_integration()
+# test_config_management()
+# test_config_driven_imports()
+# test_config_driven_generation()
+
+from tests.sample.app import app
+import fluidkit
+
+fluidkit.integrate(app)
